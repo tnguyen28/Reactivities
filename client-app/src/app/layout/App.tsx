f130e03a988a0f9e34f81d435b0f204fa5abfe2c
tnguyen28 <tntt_tyler@me.com>
2021-03-02 16:50:07 -0600

    imported assets/react front end tap in actvty data

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,30 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios'
-import { Header, List } from 'semantic-ui-react';
+import { Container, Header, List } from 'semantic-ui-react';
 import { Activity } from './models/activity';
 import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
 
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
       setActivities(response.data);
     })
   }, [])
+
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find(x => x.id === id))
+
+  }
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined)
+  }
+
   return (
-    <div>
+    <>
       <NavBar />
-      <List>
-        {activities.map((activity) => (
-          <List.Item key={activity.id}>
-            {activity.title}
-          </List.Item>
-        ))}
-
-      </List>
-
-    </div>
+      <Container style={{ marginTop: '6em' }}>
+        <ActivityDashboard activities={activities} selectedActivity={selectedActivity} selectActivity={handleSelectActivity} cancelSelectActivity={handleCancelSelectActivity} />
+      </Container>
+    </>
   );
 }
 
