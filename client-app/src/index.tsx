42c8cbc7726792b4df4a515255143ad948cd8833
tnguyen28 <tntt_tyler@me.com>
2021-03-09 13:34:41 -0600

    CSS/Activity components/Calendar

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
-
+import 'react-calendar/dist/Calendar.css'
 import 'semantic-ui-css/semantic.min.css';
 import { store, StoreContext } from './app/stores/store';
 import { BrowserRouter } from 'react-router-dom';
