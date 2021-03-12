1bb5467c99dac8a1d6aba626d17e38ce2783537a
tnguyen28 <tntt_tyler@me.com>
2021-03-12 16:33:05 -0600

    Backend Validation

--- API/Startup.cs
+++ API/Startup.cs
@@ -17,6 +17,7 @@ using MediatR;
 using Application.Activities;
 using Application.Core;
 using API.Extensions;
+using FluentValidation.AspNetCore;
 
 namespace API
 {
@@ -31,7 +32,9 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddFluentValidation(config => {
+                config.RegisterValidatorsFromAssemblyContaining<Create>();
+            });
             services.AddApplicationServices(this.config);
 
         }
